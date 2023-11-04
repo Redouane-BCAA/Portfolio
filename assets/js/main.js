00273fe9a2573caea2fae6bab228a1fdec281c0d
Redouane Bouchmaa <red.bouchmaa.dev@gmail.com>
2023-11-04 01:44:59 +0100

    Création card project + modif json data + css

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,38 +45,39 @@ async function fetchProjectsData() {
 }
   
   // Fonction pour créer l'article HTML à partir des données JSON
-  function createProjectElement(project) {
-    const projectElement = document.createElement('article');
-    projectElement.classList.add('projects_card', 'swiper-slide');
-  
-    projectElement.innerHTML = `
-      <img class="projects_img" src="${project.imageSrc}" alt="${project.altText}">
-      <div class="projects_card_text">
-        <span class="projects_subtitle">${project.subtitle}</span>
+function createProjectElement(project) {
+  const projectElement = document.createElement("article");
+  projectElement.classList.add("projects_card", "swiper-slide");
+
+  projectElement.innerHTML = `
+    <img class="projects_img" src="${project.imgSrc}" alt="projet 1">
+    <div class="projects_card_text">
         <h3 class="projects__title">${project.title}</h3>
+        <div class="techno_container">
+            ${project.technologies.map(techno => `<span class="projects_subtitle" id="span_techno">${techno}</span>`).join('')}
+        </div>
         <div class="projects_evaluation">
-          <div class="competance">
-            <h3>Skills developed</h3>
-            <span>${project.skillsDeveloped}</span>
-          </div>
-          <div class="difficulties">
-            <h3>Difficulties</h3>
-            <span>${project.difficulties}</span>
-          </div>
+            <div class="projects_description">
+                <h3>Description</h3>
+                <p>${project.description}</p>
+            </div>
+            <div class="projects_competences">
+                <h3>Compétences acquises</h3>
+                <ul>
+                    ${project.competences.map(competence => `<li>${competence}</li>`).join('')}
+                </ul>
+            </div>
         </div>
         <div class="projects_link_container">
-          <a href="${project.githubRepo}" class="projects_link">
-            Repo on GitHub <i class="ri-github-line"></i>
-          </a>
-          <a href="${project.onlineSite}" class="projects_link">
-            Online site <i class="ri-window-line"></i>
-          </a>
+            <a href="${project.githubLink}" class="projects_link">
+                Repo on GitHub <i class="ri-github-line"></i>
+            </a>
         </div>
-      </div>
-    `;
-  
-    return projectElement;
-  }
+    </div>
+  `;
+
+  return projectElement;
+}
   
   // Fonction pour créer et afficher les projets dans la section Projects
 async function displayProjectsSection() {
@@ -89,28 +90,28 @@ async function displayProjectsSection() {
         projectList.appendChild(projectElement);
       });
       // Propriétés swiper pour le slide
-      const swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 50,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        breakpoints: {
-          1024: {
-            slidesPerView: 1,
-            spaceBetween: 30,
-          },
-        },
-        autoplay: { delay: 5000 },
-        // mousewheel: true,
-        keyboard:true,
-        loop: true,
-      });
+      // const swiper = new Swiper(".mySwiper", {
+      //   slidesPerView: 1,
+      //   spaceBetween: 50,
+      //   pagination: {
+      //     el: ".swiper-pagination",
+      //     clickable: true,
+      //   },
+      //   navigation: {
+      //     nextEl: ".swiper-button-next",
+      //     prevEl: ".swiper-button-prev",
+      //   },
+      //   breakpoints: {
+      //     1024: {
+      //       slidesPerView: 1,
+      //       spaceBetween: 30,
+      //     },
+      //   },
+      //   autoplay: { delay: 5000 },
+      //   // mousewheel: true,
+      //   keyboard:true,
+      //   loop: true,
+      // });
     }
 }
 
